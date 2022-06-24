f1a25bac0d93e114e02ca1b0a861145cf48b956b
Yin Chu <ycrijnaard@gmail.com>
2022-06-24 02:03:01 +0200

    Setup for theme context

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 // Framework imports
-
+import React from "react";
 // Dependency imports
+// import { useDarkMode } from "usehooks-ts";
 
 // Type + Interface imports
 
@@ -15,14 +16,30 @@
 // Style imports
 
 const Footer = () => {
+  // const { isDarkMode, toggle, enable, disable } = useDarkMode();
+
   return (
     <>
       <h1 className="text-xl text-emerald-500">Footer component</h1>
 
-      <button className="flex items-center justify-center border border-black px-4 py-2">
-        <i className="ri-moon-fill ri-xl mx-1"></i>
-        <span className="mx-1">Dark</span>
-      </button>
+      {/* <p>Current theme: {isDarkMode ? "dark" : "light"}</p> */}
+
+      {/* <button
+        className="flex items-center justify-center border border-green-500 px-4 py-2 dark:border-red-500"
+        onClick={toggle}
+      >
+        {isDarkMode ? (
+          <>
+            <i className="ri-sun-fill ri-xl mx-1"></i>
+            <span className="mx-1">Light</span>
+          </>
+        ) : (
+          <>
+            <i className="ri-moon-fill ri-xl mx-1"></i>
+            <span className="mx-1">Dark</span>
+          </>
+        )}
+      </button> */}
     </>
   );
 };
