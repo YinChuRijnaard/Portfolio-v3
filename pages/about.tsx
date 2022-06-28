304bf394d34dbc81b9578f9955b12307ff9e2eba
Yin Chu <ycrijnaard@gmail.com>
2022-06-28 16:29:46 +0200

    Added pages

---
+++ pages/about.tsx
@@ -0,0 +1,25 @@
+// Framework imports
+
+// Dependency imports
+
+// Type + Interface imports
+
+// Context imports
+
+// Util imports
+
+// Component imports
+
+// Asset imports
+
+// Style imports
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-pink-500 dark:text-red-500">About page</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/contact.tsx
@@ -0,0 +1,25 @@
+// Framework imports
+
+// Dependency imports
+
+// Type + Interface imports
+
+// Context imports
+
+// Util imports
+
+// Component imports
+
+// Asset imports
+
+// Style imports
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-pink-500 dark:text-red-500">Contact page</h1>
+    </div>
+  );
+};
+
+export default Contact;
