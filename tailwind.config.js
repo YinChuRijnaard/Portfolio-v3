4a5274a9e91960197534bd5d2120856dc7f74998
Yin Chu <ycrijnaard@gmail.com>
2022-07-10 19:54:33 +0200

    Extended styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Merriweather Sans"],
+        serif: ["Merriweather"],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
