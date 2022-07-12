53c873552d81f6ca700e94389fa800b76fa69ddf
Yin Chu <ycrijnaard@gmail.com>
2022-07-12 16:10:26 +0200

    Test menu

---
+++ components/Menu.tsx
@@ -0,0 +1,28 @@
+// Framework imports
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <main className="absolute z-10 h-screen w-screen rounded bg-neutral-100 p-32 font-serif dark:bg-neutral-900">
+      <ul className="space-y-16 text-center text-6xl">
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/work">
+            <a>Work</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default Menu;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,31 @@
+// Framework imports
+import { useState } from "react";
+
 // Dependency imports
 import { Squeeze as Hamburger } from "hamburger-react";
 
 // Component imports
+import Menu from "../components/Menu";
 import Logo from "./Logo";
 
 const Navbar = () => {
-  return (
-    <nav className="mx-4 flex items-center justify-between md:mx-32 lg:mx-64">
-      <Logo />
+  const [open, setOpen] = useState<boolean>(false);
 
-      <Hamburger distance="lg" color="#dc2626" label="Show menu" rounded />
-      {/* color: red-500 */}
-    </nav>
+  const handleOpen = () => setOpen(!open);
+
+  return (
+    <>
+      <nav className="mx-4 flex items-center justify-between md:mx-32 lg:mx-64">
+        <Logo />
+
+        <button onClick={handleOpen}>
+          <Hamburger distance="lg" color="#dc2626" label="Show menu" rounded />
+          {/* color: red-500 */}
+        </button>
+      </nav>
+
+      {open ? <Menu /> : null}
+    </>
   );
 };
 
