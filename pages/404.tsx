f58192b2a4e4a73c86eb05352878642a0232e180
Yin Chu <ycrijnaard@gmail.com>
2022-07-12 14:46:45 +0200

    Added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+// Framework imports
+import type { NextPage } from "next";
+
+const ErrorPage: NextPage = () => {
+  return (
+    <main>
+      <h1>Page not found</h1>
+    </main>
+  );
+};
+
+export default ErrorPage;
