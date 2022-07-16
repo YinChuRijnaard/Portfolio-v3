5fbb2c85f4a02e755ab7b07bddfe21a9a03ac82c
Yin Chu <ycrijnaard@gmail.com>
2022-07-16 12:34:43 +0200

    Test Typeform for mobile

--- components/TypeformForm.tsx
+++ components/TypeformForm.tsx
@@ -1,9 +1,17 @@
 // Dependency imports
-import { Widget } from "@typeform/embed-react";
+import { PopupButton, Widget } from "@typeform/embed-react";
 
 const TypeformForm = () => {
   const FORM_ID: string = "displWaE";
-  return <Widget id={FORM_ID} style={{ width: "100%" }} className="my-form" />;
+
+  return (
+    <>
+      <PopupButton id={FORM_ID} style={{ fontSize: 20 }} className="my-button">
+        click to open form in popup
+      </PopupButton>
+      {/* <Widget id={FORM_ID} style={{ width: "100%", height: "100%" }} className="h-screen w-screen" /> */}
+    </>
+  );
 };
 
 export default TypeformForm;
